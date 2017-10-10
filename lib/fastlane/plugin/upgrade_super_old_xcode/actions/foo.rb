4b6d0d335278fcb037bc9d05af853d8b76316bdf
Jan Piotrowski <piotrowski@gmail.com>
2017-10-10 16:59:51 +0200

    understand and add comments to original code resort some lines that were not really in the right place (also fix some typos in original comments)

--- lib/fastlane/plugin/upgrade_super_old_xcode/actions/foo.rb
+++ lib/fastlane/plugin/upgrade_super_old_xcode/actions/foo.rb
@@ -3,25 +3,35 @@ module Fastlane
   module Actions
     class UpdateProjectCodesigningAction < Action
       def self.run(params)
+        # print params
         FastlaneCore::PrintTable.print_values(config: params, title: "Summary for Update Project Codesigning")
+        
+        # create project.pbxproj path and check if exists
         path = params[:path]
         path = File.join(File.expand_path(path), "project.pbxproj")
-
-        project = Xcodeproj::Project.open(params[:path])
         UI.user_error!("Could not find path to project config '#{path}'. Pass the path to your project (not workspace)!") unless File.exist?(path)
         UI.message("Updating the Automatic Codesigning flag to #{params[:use_automatic_signing] ? 'enabled' : 'disabled'} for the given project '#{path}'")
+        
+        # open project
+        project = Xcodeproj::Project.open(params[:path])
 
+        # upgrade super old xcode project
+        # if there is no TargetAttributes
         unless project.root_object.attributes["TargetAttributes"]
           UI.error("Seems to be a very old project file format")
           UI.error("PLEASE BACKUP ALL FILES before doing this.")
+
+          # prompt for confirmation or look for ENV variable
           if ENV["FL_PROJECT_SIGNING_FORCE_UPGRADE"] || UI.confirm("Proceed with upgrade to xcode8 format?")
             UI.important("Upgrading project to use xcode8 signing stuff")
+            
+            # exit if no team_id param
             unless params[:team_id]
               UI.important("TEAM id is not set")
               UI.error!("Provide :team_id")
             end
 
-            # set upgrade market to xcdoe8
+            # set upgrade marker to xcode8
             project.root_object.attributes["LastUpgradeCheck"] = "0800"
             target_attr_hash = {}
 
@@ -31,19 +41,25 @@ module Fastlane
               new_hash = {}
               new_hash["CreatedOnToolsVersion"] = "8.0"
               new_hash["DevelopmentTeam"] = params[:team_id]
-              new_hash["ProvisioningStyle"] = params[:use_automatic_signing] ? 'Automatic' : 'Manual'
+              new_hash["ProvisioningStyle"] = params[:use_automatic_signing] ? 'Automatic' : 'Manual' # TODO Do we need this as param? Fixed? To what of the two values?
               target_attr_hash[target.uuid] = new_hash
             end
+            project.root_object.attributes["TargetAttributes"] = target_attr_hash
 
             # for each configuration set a signing identity
+            # TODO what does this actually do? Does this directly change the `project`? (Oh no, have to learn some ruby to understand...)
             project.build_configurations.each do |config|
               config.build_settings['CODE_SIGN_IDENTITY[sdk=iphoneos*]'] = config.name == "Release" ? 'iPhone Distribution' : "iPhone Development"
             end
-            project.root_object.attributes["TargetAttributes"] = target_attr_hash
+            
           else
             UI.user_error!("canceled upgrade")
           end
         end
+
+        # other stuff from update_project_codesigning
+        # now can be found and executed via https://docs.fastlane.tools/actions/automatic_code_signing/
+        # Source: https://github.com/fastlane/fastlane/blob/master/fastlane/lib/fastlane/actions/automatic_code_signing.rb
 
         target_dictionary = project.targets.map { |f| { name: f.name, uuid: f.uuid } }
         changed_targets = []
@@ -94,16 +110,17 @@ module Fastlane
                                        verify_block: proc do |value|
                                          UI.user_error!("Path is invalid") unless File.exist?(File.expand_path(value))
                                        end),
+          FastlaneCore::ConfigItem.new(key: :team_id,
+                                       env_name: "FASTLANE_TEAM_ID",
+                                       optional: true,
+                                       description: "Team ID, is used when upgrading project",
+                                       is_string: true),
+
           FastlaneCore::ConfigItem.new(key: :use_automatic_signing,
                                        env_name: "FL_PROJECT_USE_AUTOMATIC_SIGNING",
                                        description: "Defines if project should use automatic signing",
                                        is_string: false,
                                        default_value: false),
-          FastlaneCore::ConfigItem.new(key: :team_id,
-                                        env_name: "FASTLANE_TEAM_ID",
-                                        optional: true,
-                                        description: "Team ID, is used when upgrading project",
-                                        is_string: true),
           FastlaneCore::ConfigItem.new(key: :targets,
                                        env_name: "FL_PROJECT_SIGNING_TARGETS",
                                        optional: true,
